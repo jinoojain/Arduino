1eccb571ed3c1809fe37cedf1a68c4bb5c357c23
Rundong Tian <rutian@berkeley.edu>
2013-07-19 12:18:18 -0700

    try to control motor through serial commands

---
+++ motorTest/motorTest.ino
@@ -0,0 +1,39 @@
+//MotorTest
+
+int pwmPin = 6; // PWM for our single motor for now
+int dirPin = 8; // toggle direction
+int value;
+
+int power;
+void setup() {
+ 
+  pinMode(pwmPin,OUTPUT);
+  pinMode(dirPin,OUTPUT);
+  
+  Serial.begin(9600);
+  
+}
+void loop() {  
+  
+  if(Serial.available()){
+   value = Serial.read();
+   
+   
+   if(value >= 0 && value <= 255){
+      analogWrite(pwmPin, value);
+      digitalWrite(dirPin,HIGH);
+   }
+   else if (value < 0 && value >= -255){
+      analogWrite(pwmPin, value);
+      digitalWrite(dirPin,LOW);
+   }
+   else{
+     Serial.println("please pick valid value");
+   }
+   
+  }// end if
+  
+  Serial.print("inputed value: ");
+  Serial.println(value);
+  delay(300);
+}
