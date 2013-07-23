2eb3b7a95912f57c45fca397d9add93bb19ff368
Rundong Tian <rutian@berkeley.edu>
2013-07-22 18:36:38 -0700

    added xbee link in helpful docs

--- simpleMotorTest/simpleMotorTest.ino
+++ simpleMotorTest/simpleMotorTest.ino
@@ -1,21 +1,20 @@
 //MotorTest
 
-int pwmPin = 9; // PWM for our single motor for now
+int pwmPin = 9; // PWM for our single motor
 int dirPin = 8; // toggle direction
 
 
 void setup() {
  
-  //pinMode(pwmPin,OUTPUT);
+  pinMode(pwmPin,OUTPUT);
   pinMode(dirPin,OUTPUT);
   
-  Serial.begin(9600);
-  
+
 }
 void loop() {  
-  analogWrite(pwmPin, 150);
+    analogWrite(pwmPin, 90);
   //digitalWrite(pwmPin,HIGH);
-  digitalWrite(dirPin,HIGH);
+  digitalWrite(dirPin,HIGH);  
 
   delay(300);
 }
