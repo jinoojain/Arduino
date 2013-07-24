d37f0e46ef744c0f6599017c27f0667700af4373
Jinoo Jain <jinoojain@berkeley.edu>
2013-07-23 17:53:22 -0700

    Update motorTest.ino
    Updated code to handle basic remote controls

--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -15,25 +15,46 @@ void setup() {
 }
 void loop() {  
   
-  if(Serial.available()){
-   value = Serial.read();
+  while(Serial.available()){
+   char value = Serial.read();
    
-   
-   if(value >= 0 && value <= 255){
-      analogWrite(pwmPin, value);
-      digitalWrite(dirPin,HIGH);
+   if (value=='q'||value=='w'||value=='e'){
+     digitalWrite(dirPin, HIGH);
+     if (value=='q'){
+       analogWrite(pwmPin, 50);
+     }
+     if (value=='w'){
+       analogWrite(pwnPin, 100);
+     }
+     else{
+       analogWrite(pwmPin, 200);
+     }
    }
-   else if (value < 0 && value >= -255){
-      analogWrite(pwmPin, value);
-      digitalWrite(dirPin,LOW);
+   if (value=='a'||value=='s'||value=='d'){
+     digitalWrite(dirPin, LOW);
+     if (value=='a'){
+       analogWrite(pwmPin, 50);
+     }
+     if (value=='s'){
+       analogWrite(pwnPin, 100);
+     }
+     else{
+       analogWrite(pwmPin, 200);
+     }
+   }
+   if (value == 'x'){
+     digitalWrite(dirPin, HIGH);
+     analogWrite(pwmPin,0);
    }
    else{
-     Serial.println("please pick valid value");
+     Serial.println("Unknown Command");
    }
    
-  }// end if
-  
+  }    // end while
   Serial.print("inputed value: ");
   Serial.println(value);
-  delay(300);
+  
 }
+
+
+
